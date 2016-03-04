ac89be9e8bdcf5f7c041d6ebd11c27910e2bc811
Luke Horvat <lukehorvat@gmail.com>
2016-03-04 15:19:32 +1100

    Simplify

--- lib/initialize/patch.rb
+++ lib/initialize/patch.rb
@@ -3,30 +3,9 @@ module Initialize
     def run(*args)
       if @name == :load_config_initializers
         @block = lambda do |*args|
-          initializers_path = "config/initializers"
-
-          initializers = config.paths[initializers_path].existent.sort
-
-          initializer_dependencies = Hash[
-            config.initializer_dependencies.map do |initializer, dependencies|
-              [
-                "#{config.root}/#{initializers_path}/#{initializer}.rb",
-                dependencies.map do |dependency|
-                  "#{config.root}/#{initializers_path}/#{dependency}.rb"
-                end
-              ]
-            end
-          ]
-
-          independent_initializers = initializers.select do |initializer|
-            !initializer_dependencies.key?(initializer)
-          end
-
-          dependent_initializers = initializers - independent_initializers
-
-          initializers = independent_initializers + dependent_initializers
-
-          initializers.each do |initializer|
+          config.initializers.map do |initializer|
+            "#{config.root}/config/initializers/#{initializer}.rb"
+          end.each do |initializer|
             load_config_initializer(initializer)
           end
         end
